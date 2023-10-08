a3ce31053f478943888ad27e9172725c31cb3b2c
Qsen02 <diyangenchev73@gmail.com>
2023-10-08 16:52:20 +0300

    Добавена нова задача

---
+++ functions-more exrecise/Print DNA.js
@@ -0,0 +1,26 @@
+function printDNA(num) {
+    printsCount = 0;
+    for (let i = 1; i < num + 1; i++) {
+        let curLine = printStarsAndLetters(i);
+        let curLetter = curLine.split(" ");
+        let firstLetter = curLetter[0];
+        let secondLetter = curLetter[1];
+        if (i % 2 == 0) {
+            console.log(`*${firstLetter}--${secondLetter}*`);
+        } else {
+            printsCount++;
+            if (printsCount % 2 != 0) {
+                console.log(`**${firstLetter}${secondLetter}**`);
+            } else {
+                console.log(`${firstLetter}----${secondLetter}`);
+            }
+        }
+    }
+}
+
+function printStarsAndLetters(num) {
+    let letters = ["A T", "C G", "T T", "A G", "G G"];
+    let length = letters.length;
+    return letters[(num - 1) % length];
+}
+printDNA(50);
